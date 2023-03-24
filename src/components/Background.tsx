79e595d98e36a21e06bd40e4bcaf53b58fe2f599
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-03-24 12:59:33 -0300

    Update background component

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,12 +1,18 @@
-import React from "react";
-import bgTop from "../images/bg-top.svg";
-import bgBottom from "../images/bg-bottom.svg";
+import { backgroundImages } from "../data";
 
 export function Background() {
   return (
     <>
-      <img className="absolute -z-50 -top-8 -right-2/4" src={bgTop} alt="" />
-      <img className="absolute -z-50 hidden" src={bgBottom} alt="" />
+      <img
+        className="absolute -z-50 -top-8 -right-2/4 lg:top-0 lg:right-0"
+        src={backgroundImages.backgroundTop.image}
+        alt={backgroundImages.backgroundTop.alt}
+      />
+      <img
+        className="absolute -z-50 hidden lg:block lg:bottom-0 lg:left-0"
+        src={backgroundImages.backgroundBottom.image}
+        alt={backgroundImages.backgroundBottom.alt}
+      />
     </>
   );
 }
---
+++ src/data/index.ts
@@ -0,0 +1,13 @@
+import bgTop from "../images/bg-top.svg";
+import bgBottom from "../images/bg-bottom.svg";
+
+export const backgroundImages = {
+  backgroundTop: {
+    image: bgTop,
+    alt: "background top",
+  },
+  backgroundBottom: {
+    image: bgBottom,
+    alt: "background bottom",
+  },
+};
