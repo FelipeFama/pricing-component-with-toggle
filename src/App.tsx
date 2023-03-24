d3cae808becd8817af433632f8ff2f9a629a29aa
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-03-23 22:23:12 -0300

    feat: toggle component style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import bgTop from "./images/bg-top.svg";
 import bgBottom from "./images/bg-bottom.svg";
+import { Toggle } from "./components/Toggle";
 
 export function App() {
   return (
     <main className="relative z-0 overflow-hidden min-h-screen font-montserrat text-15 text-grayishBlue bg-veryLightGrayishBlue px-6 py-6">
-      <img src={bgTop} alt="" />
-      <img src={bgBottom} alt="" />
+      <img className="absolute -z-50 -top-8 -right-2/4" src={bgTop} alt="" />
+      <img className="absolute -z-50 " src={bgBottom} alt="" />
+      <Toggle />
     </main>
   );
 }
--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -2,8 +2,20 @@ import React from "react";
 
 export function Toggle() {
   return (
-    <header>
-      <nav></nav>
+    <header className="flex items-center">
+      <h1 className="text-32 text-center">Our Pricing</h1>
+      <nav className="flex items-center justify-center">
+        <p>Anually</p>
+        <label className="block relative h-8 w-14" htmlFor="toggleBtn"></label>
+        <input
+          className="invisible"
+          type="checkbox"
+          name="toggleBtn"
+          id="toggleBtn"
+        />
+        <span></span>
+        <p>Monthly</p>
+      </nav>
     </header>
   );
 }
