e4b9ba2c492d649873723a20ff73565918416b1d
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-03-24 02:36:10 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,20 @@ export function App() {
         <Card
           title="Basic"
           price={priceMonthly ? 19.99 : 199.99}
+          whatYouGet={["500 GB Storage", "2 Users Allowed", "Send up to 3 GB"]}
+        />
+        <Card
+          title="Professional"
+          price={priceMonthly ? 24.99 : 249.99}
           whatYouGet={["1 TB Storage", "5 Users Allowed", "Send up to 10 GB"]}
+          bgColor="bg-gradient-to-br from-linearGradient-from to-linearGradient-to"
+          textColor="text-white"
+          priceColor="text-white"
+        />
+        <Card
+          title="master"
+          price={priceMonthly ? 39.99 : 399.0}
+          whatYouGet={["2 TB Storage", "10 Users Allowed", "Send up to 20 GB"]}
         />
       </section>
     </main>
