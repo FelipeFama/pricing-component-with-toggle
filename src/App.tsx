8411eb5c89d8a26b74166170efdba8aed9a5f009
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-03-23 21:55:16 -0300

    feat: add new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import bgTop from "./images/bg-top.svg";
+import bgBottom from "./images/bg-bottom.svg";
 
 export function App() {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <main className="relative z-0 overflow-hidden min-h-screen font-montserrat text-15 text-grayishBlue bg-veryLightGrayishBlue px-6 py-6">
+      <img src={bgTop} alt="" />
+      <img src={bgBottom} alt="" />
+    </main>
   );
 }
