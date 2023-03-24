9ea58fdfb37151c0cf6adc814d87b16f7c642ede
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-03-23 21:41:57 -0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,20 @@ module.exports = {
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
       },
+      fontSize: {
+        15: "0.9375rem",
+        32: "2rem",
+      },
+      colors: {
+        veryLightGrayishBlue: "hsl(240, 78%, 98%)",
+        lightGrayishBlue: "hsl(234, 14%, 74%)",
+        grayishBlue: "hsl(233, 13%, 49%)",
+        darkGrayishBlue: "hsl(232, 13%, 33%)",
+        linearGradient: {
+          from: "hsl(236, 72%, 79%)",
+          to: "hsl(237, 63%, 64%)",
+        },
+      },
     },
   },
   plugins: [],
