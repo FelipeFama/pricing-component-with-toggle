b73829fe770cb13e5499fd6f9e8ecfa4e3b78e3c
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-03-24 02:12:52 -0300

    feat: styling card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,19 +13,24 @@ export function Card({
   title,
   price,
   whatYouGet,
-  bgColor,
-  textColor,
-  priceColor,
+  bgColor = "bg-white",
+  textColor = "text-grayishBlue",
+  priceColor = "text-darkGrayishBlue",
 }: CardProps) {
   return (
-    <section>
-      <p></p>
-      <h2></h2>
-      <article>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+    <section className="">
+      <p className="mb-9 text-lg">{title}</p>
+      <h2
+        className={`text-7xl flex items-center justify-center mb-9 -tracking-[0.1125rem] ${priceColor}`}
+      >
+        <span className="text-[2.5rem]">$</span>
+        {price}
+      </h2>
+      <article className="mb-8">
+        <ul className="border-y-1">
+          <li className="py-4 border-b-1">{whatYouGet[0]}</li>
+          <li className="py-4 border-b-1">{whatYouGet[1]}</li>
+          <li className="py-4">{whatYouGet[2]}</li>
         </ul>
       </article>
       <button>LEARN MORE</button>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      borderWidth: {
+        1: "1px",
+      },
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
       },
